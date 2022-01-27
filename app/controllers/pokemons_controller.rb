78a46a5c074f6c66e126e10da426af346dd8a3ec
Gabriel Rendon Paredes <gabriel.rendon@wizeline.com>
2022-01-27 05:27:54 -0600

    Fix missing style

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,7 +1,7 @@
 class PokemonsController < ApplicationController
   before_action :authenticate_trainer!, except: %i[index]
   before_action :set_pokemon, only: %i[show edit update destroy]
-  load_and_authorize_resource only: %i[edit destroy]
+  load_and_authorize_resource except: %i[index show]
 
   # GET /pokemons or /pokemons.json
   def index
